9344697e035affd0f100ce8a90fe0a3898f98e5c
Lukas Urba <lukas.urba@gmail.com>
2024-06-03 23:51:09 +0100

    Cleaned and commented code

--- src/Main.java
+++ src/Main.java
@@ -5,10 +5,12 @@ public class Main {
     }
 
     public static int appendCharacters(String s, String t) {
-        int sPos = 0;
-        int tPos = 0;
+//        Creating increments to manipulate position within the while loop
+        int sPos = 0, tPos = 0 ;
 
+//        Both strings need to be not traversed through, otherwise it will be out of bounds comparison
         while(sPos<s.length()&&tPos<t.length()) {
+//            if any of the characters match, they do not need to be appended
             if (s.charAt(sPos)==t.charAt(tPos)) {
                 sPos++;
                 tPos++;
@@ -16,6 +18,7 @@ public class Main {
                 sPos++;
             }
         }
+//        returns the amount left in second string that needs to be appended
         return t.length()-tPos;
 
     }
