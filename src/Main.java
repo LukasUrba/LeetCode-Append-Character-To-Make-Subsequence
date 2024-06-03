a63877d27b1c16ec984884e9b59c657f73d0b344
Lukas Urba <lukas.urba@gmail.com>
2024-06-03 23:44:00 +0100

    Working example of returning the amount of characters needed to append to a string to make it a subsequence

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println(appendCharacters("coaching","coding"));
+
+    }
+
+    public static int appendCharacters(String s, String t) {
+        int sPos = 0;
+        int tPos = 0;
+
+        while(sPos<s.length()&&tPos<t.length()) {
+            if (s.charAt(sPos)==t.charAt(tPos)) {
+                sPos++;
+                tPos++;
+            } else {
+                sPos++;
+            }
+        }
+        return t.length()-tPos;
+
     }
 }
