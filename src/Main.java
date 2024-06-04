cc3fa1766e5676633a6f1d67a3730582f4ae8da2
Lukas Urba <lukas.urba@gmail.com>
2024-06-04 21:54:05 +0100

    Made variables for s and t lengths for shorter times

--- src/Main.java
+++ src/Main.java
@@ -6,20 +6,18 @@ public class Main {
 
     public static int appendCharacters(String s, String t) {
 //        Creating increments to manipulate position within the while loop
-        int sPos = 0, tPos = 0 ;
+        int sPos = 0, tPos = 0, sLength = s.length(), tLength = t.length();
 
 //        Both strings need to be not traversed through, otherwise it will be out of bounds comparison
-        while(sPos<s.length()&&tPos<t.length()) {
+        while(sPos<sLength&&tPos<tLength) {
 //            if any of the characters match, they do not need to be appended
             if (s.charAt(sPos)==t.charAt(tPos)) {
-                sPos++;
                 tPos++;
-            } else {
-                sPos++;
             }
+            sPos++;
         }
 //        returns the amount left in second string that needs to be appended
-        return t.length()-tPos;
+        return tLength-tPos;
 
     }
 }
